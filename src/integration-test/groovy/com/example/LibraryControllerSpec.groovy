bdca1652b301b4dbbeddc794d56f0867cbd26780
Andrew Thigpen <athigpen@circadence.com>
2015-11-13 15:28:45 -0600

    HibernateException integration test issue.

---
+++ src/integration-test/groovy/com/example/LibraryControllerSpec.groovy
@@ -0,0 +1,39 @@
+package com.example
+
+
+import grails.test.mixin.integration.Integration
+import grails.transaction.*
+import spock.lang.*
+
+@Integration
+@Rollback
+class LibraryControllerSpec extends Specification {
+
+  def library
+
+  def setup() {
+  }
+
+  def cleanup() {
+  }
+
+  def setupData() {
+    library = new Library(name: 'library')
+    library.save(flush: true, failOnError: true)
+  }
+
+  // This test fails with a 'No Session found in current thread'
+  // HibernateException if the following lines are uncommented in build.gradle.
+  // Without these lines, it passes.
+  //
+  //   compile "org.jadira.usertype:usertype.extended:4.0.0.GA"
+  //   compile "org.hibernate:hibernate-entitymanager"
+  //
+  void "test save"() {
+    given:
+    setupData()
+
+    expect:
+    library != null
+  }
+}
